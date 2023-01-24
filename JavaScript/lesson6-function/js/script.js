ef85b7e4e069665eaeb5c4f674a226019053f070
Hasan Bakhtiar <hasan.doctype@gmail.com>
2023-01-24 10:58:37 +0400

    function

---
+++ JavaScript/lesson6-function/js/script.js
@@ -0,0 +1,111 @@
+// function name(params){
+    // codes
+// }
+
+// function calculate(numberOne,numberTwo){
+//         const total = numberOne+numberTwo;
+//         console.log(total);
+// }
+// calculate(10,20)
+// calculate(40,20)
+// calculate(100,90)
+// calculate(10,90)
+// calculate(100,10)
+
+
+// function sayHi (){
+//     console.log('hello');
+// }
+
+// sayHi();
+
+
+// const action = function(){
+//     console.log("action function is running...");
+// }
+// action()
+
+
+// const myObj ={
+//     title:"BMW",
+//     model:"X5",
+//     calculateSpeed:function(km,hour){
+//         console.log(km/hour+"km/h");
+//     }
+// }
+
+// myObj.calculateSpeed(100,2)
+
+
+// function myFunc (){
+//     return 'Hello'
+// }
+// console.log(myFunc());
+
+
+// arrow function
+// ()=>{}
+// const myFunc = ()=>{
+//         return 'this is arrow function'
+// }
+// console.log(myFunc());
+
+// (function(name,surname){
+//     console.log(name,surname);
+// })("Hasan","Bakhtiar")
+
+
+// Not a number
+
+// const myData=(comingData)=>{
+//     return comingData;
+// }
+
+// const totalData =(title,currentPrice,oldPrice)=>{
+//         let middlePrice;
+//         if (currentPrice > oldPrice) {
+//              middlePrice = "Product name:"+ title +" "+ (currentPrice-oldPrice)+"$";
+//         }else{
+//             middlePrice = 'something is wrong'
+//         }
+
+//         console.log(myData(middlePrice));
+
+// }
+
+// totalData('Pen',10,8);
+
+
+
+// 10km = 4l 
+
+// const distance=(fuel)=>{
+//     const oneFuelToDistance = 10/4;
+//     const total = fuel * oneFuelToDistance;
+//     return total +"km/h";
+        
+// }
+// console.log(distance(5));
+
+
+
+
+// const car =(brand="Mercedes",model="S500",km="100",hour="5")=>{
+//     return brand+" "+model+" " + (km/hour)+"km/h";
+// }
+
+// console.log(car("BMW","X5",100,2));
+
+
+
+// const recursiveFunc=()=>{
+//     recursiveFunc();
+// }
+
+
+const text = "JavaScript";
+
+console.log(text.match(/a/g));
+
+
+
