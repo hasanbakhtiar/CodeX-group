5756cfa641d2cb602b36cc1282cdbd1ed61c5f21
Hasan Bakhtiar <hasan.doctype@gmail.com>
2023-02-07 10:37:34 +0400

    dom part2

---
+++ JavaScript/lesson10-DOMpart2/js/script.js
@@ -0,0 +1,30 @@
+// const data = [
+//   {
+//     id: 1,
+//     title: "pen",
+//   },
+//   {
+//     id:2,
+//     title:"book"
+//   }
+// ];
+
+
+// data.map((item)=>{
+//   console.log(item.title);
+// })
+
+
+// document.querySelector("h1").attributes[1].value = "red";
+// document.querySelector('h1').className = "big"
+// document.querySelector('h1').classList.add('little')
+// document.querySelector('h1').classList.remove('big');
+
+
+// document.querySelector('button').onclick=()=>{
+//     document.querySelector('h1').classList.toggle('big');
+// }
+
+document.querySelector('.bar').onclick=()=>{
+    document.querySelector('ul').classList.toggle('show');
+}
