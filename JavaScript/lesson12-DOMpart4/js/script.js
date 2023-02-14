927614c33723e49412b0d95f782e1623158cbac3
Hasan Bakhtiar <hasan.doctype@gmail.com>
2023-02-14 10:33:29 +0400

    dom node

---
+++ JavaScript/lesson12-DOMpart4/js/script.js
@@ -0,0 +1,68 @@
+
+
+
+// const text = document.querySelector('p');
+// const list = document.querySelector('ul');
+// text.innerHTML = list.childNodes[1].previousSibling.firstChild.nodeType;
+
+
+// const myNewTag = document.createElement('h1');
+// const myNewText = document.createTextNode('Hello JavaScript');
+// myNewTag.appendChild(myNewText);
+// document.body.appendChild(myNewTag);
+
+
+// const btn = document.querySelector('button');
+// const form  = document.querySelector('form');
+// const input = document.querySelector('input')
+// const list = document.querySelector('ol');
+
+
+// input.onkeyup =()=>{
+//     input.style.height = '20px';
+//     input.style.backgroundColor = 'white';
+//     input.style.transition = '1s'
+// }
+
+// input.onkeydown =()=>{
+//     input.style.height = '70px';
+//     input.style.backgroundColor = 'red';
+//     input.style.transition = '1s'
+// }
+
+
+
+// const todoApp =e=>{
+//     e.preventDefault();
+// if (!input.value || input.value === ' ') {
+//     alert('please,fill input');
+// }else{
+//     const li  = document.createElement('li');
+//     li.innerHTML = input.value.trim();
+//     list.appendChild(li);
+//     input.value = "";
+//     console.log(li.innerHTML);
+//     li.onclick = ()=>{
+//         if (li.style.textDecoration === 'none') {
+//             li.style.textDecoration = "line-through";
+//         }else{
+//             li.style.textDecoration = "none"
+//         }
+//     }
+//     li.ondblclick=()=>{
+//         // list.removeChild(li);
+//         li.remove();
+//     }
+// }
+   
+// }
+
+
+// form.onsubmit = todoApp;
+
+
+
+const defaultMail = "info@example.com";
+
+
+
