4bc9ff0d9a8d51a18516a8a24bab815a300a3771
Hasan Bakhtiar <hasan.doctype@gmail.com>
2023-05-09 12:05:48 +0400

    redux enter

---
+++ JavaScript/ReduxJS/lesson1-introduction/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <h1>Hello Redux JS</h1>
+  </React.StrictMode>
+);
+
