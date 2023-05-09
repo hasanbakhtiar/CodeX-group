2e94878250342da58d3181ee1a0e63f6dc19021b
Hasan Bakhtiar <hasan.doctype@gmail.com>
2023-05-09 12:23:44 +0400

    redux enter

--- JavaScript/ReduxJS/lesson1-introduction/src/index.js
+++ JavaScript/ReduxJS/lesson1-introduction/src/index.js
@@ -1,5 +1,46 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createStore } from 'redux';
+
+
+
+const initalState = 0;
+
+const store = createStore((state = initalState, action) => {
+  switch (action.type) {
+
+    case 'increment':
+      return state + 1
+
+    case 'decrement':
+      return state - 1
+
+    default:
+      return 0
+  }
+});
+
+
+store.subscribe(() => {
+  console.log(store.getState())
+})
+
+store.dispatch({
+  type:"increment"
+})
+store.dispatch({
+  type:"decrement"
+})
+store.dispatch({
+  type:"decrement"
+})
+
+
+
+
+
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
