d9db5e044ff20cc624c3906729d505d11551f906
Hasan Bakhtiar <hasan.doctype@gmail.com>
2023-05-23 14:32:30 +0400

    blog part 5

--- JavaScript/ReduxJS/lesson6-blogapp-firebase/src/redux/store/configureStore.js
+++ JavaScript/ReduxJS/lesson6-blogapp-firebase/src/redux/store/configureStore.js
@@ -5,7 +5,7 @@ import thunk from 'redux-thunk';
 const configureStore = ()=>{
     const store = createStore(blogReducer,compose(applyMiddleware(thunk)));
     
-    return store;k
+    return store;
 }
 export default configureStore;
 
