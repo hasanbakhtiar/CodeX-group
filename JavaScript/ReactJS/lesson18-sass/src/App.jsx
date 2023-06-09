098041526d74954fa9edea657f348ffbe9f893da
Hasan Bakhtiar <hasan.doctype@gmail.com>
2023-06-09 12:28:04 +0400

    sass

---
+++ JavaScript/ReactJS/lesson18-sass/src/App.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1>hello</h1>
+      <ol>
+        <li>test</li>
+        <li>test</li>
+        <li>test</li>
+        <li>test</li>
+        <li>test</li>
+        <li>test</li>
+        <li>test</li>
+        <li>test</li>
+        <li>test</li>
+        <li>test</li>
+        <li>test</li>
+        <li>test</li>
+        <li>test</li>
+        <li>test</li>
+        <li>test</li>
+      </ol>
+
+    </div>
+  )
+}
+
+export default App
